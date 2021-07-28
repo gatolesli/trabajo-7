1903a0387f97080c51ab1413e0c2ee101c1b1773
gatolesli <87496278+gatolesli@users.noreply.github.com>
2021-07-28 15:19:39 -0500

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,18 @@
+<! DOCTYPE html >
+<  html  >
+
+<  cabeza  >
+	<  t ítulo  > Parcela Sandbox </  t ítulo >
+	<  meta   charset = " UTF-8 " />
+</  cabeza  >
+
+<  cuerpo  >
+
+		<  div   id = " aplicación " >  </  div  >
+	<  input   id = " input_pregunta " >   </  input  >
+  <  button   id = " btn_pregunta " > Ingresar respuesta </  button  >
+	<  script   src = " src / index.js " >
+	</ script>
+</ cuerpo>
+
+</ html>
